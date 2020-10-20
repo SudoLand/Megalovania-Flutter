350d7bc37b2f8ac43472b0bf497c78f74309bbff
WMXPY <wm@mengw.io>
2020-10-20 14:53:20 -0500

    add core deltelate

--- lib/entry/entry.dart
+++ lib/entry/entry.dart
@@ -1,12 +1,14 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_localizations/flutter_localizations.dart';
 import 'package:megalovania/entry/home.dart';
+import 'package:megalovania/i18n/core/localizations.dart';
 
 class EntryView extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
       localizationsDelegates: [
+        CoreLocalizations.delegate,
         GlobalMaterialLocalizations.delegate,
         GlobalWidgetsLocalizations.delegate,
       ],
