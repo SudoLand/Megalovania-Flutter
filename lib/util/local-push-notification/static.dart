84e965443262455e8e32b2fa3b9395182e6cf101
PCXPY <pc@wmpcxpy.com>
2020-10-21 12:10:28 -0500

    [chore] add static file

---
+++ lib/util/local-push-notification/static.dart
@@ -0,0 +1,4 @@
+const String defaultAndroidChannelId = "local-push-notification";
+const String defaultAndroidChannelName = "local-push-notification";
+const String defaultAndroidChannelDescription =
+    "local-push-notification-default-channel";
